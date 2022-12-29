1ae187ee155c99770db59c72449b7490d379d58a
Marwan1998 <Dragonkingofhell@gmail.com>
2022-12-29 14:57:17 +0200

    inti files

--- app/Http/Controllers/API/UserAuthAPIController.php
+++ app/Http/Controllers/API/UserAuthAPIController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class UserAuthAPIController extends Controller
 {
-    //
+
+    
+    //post request
+    public function login(Request $request)
+    {
+        return Response::json(['message' => 'login'], 200);
+    }
+
+    //post request
+    public function register(Request $request)
+    {
+        return Response::json(['message' => 'register'], 200);
+    }
+
+    //get request
+    public function logout(Request $request)
+    {
+        return Response::json(['message' => 'logout'], 200);
+    }
+
+    //get request
+    public function get_info(Request $request)
+    {
+        return Response::json(['message' => 'get_info'], 200);
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\SecretAPIController;
-use App\Http\Controllers\API\ApiController;
+// use App\Http\Controllers\API\ApiController;
+use App\Http\Controllers\API\UserAuthAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +18,16 @@ use App\Http\Controllers\API\ApiController;
 */
 
 
-Route::post('login', [ApiController::class, 'authenticate']);
-Route::post('register', [ApiController::class, 'register']);
+Route::post('login', [UserAuthAPIController::class, 'login']);
+Route::post('register', [UserAuthAPIController::class, 'register']);
 
 Route::group(['middleware' => ['jwt.verify']], function () {
-    Route::get('logout', [ApiController::class, 'logout']);
-    Route::get('get_user', [ApiController::class, 'get_user']);
+    
+    // Route::get('logout', [ApiController::class, 'logout']);
+    // Route::get('get_user', [ApiController::class, 'get_user']);
+
+    Route::get('logout', [UserAuthAPIController::class, 'logout']);
+    Route::get('get_info', [UserAuthAPIController::class, 'get_info']);
     
     Route::resource('secrets', SecretAPIController::class);
 
